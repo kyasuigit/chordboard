39466403cfeff816d2d1d6b51c3b1c7f325d2793
Terry <t.juottawa@gmail.com>
2021-11-24 02:29:49 -0500

    displays several chords now per one note

--- main.cpp
+++ main.cpp
@@ -6,17 +6,31 @@ int main(){
     // Creates a chord and note 
     Chord testChord("Major");
     Note middleC("C", 4);
+    Note middleE("E", 4);
+    Note middleF("F", 4);
     testChord.insertNote(middleC);
+    testChord.insertNote(middleE);
+    
     findChord tryMajor(testChord);
 
-    Chord majorChord = tryMajor.majorKey(middleC);
+    vector<Chord> majorChord = tryMajor.findMajor(testChord);
 
-    vector<Note> Notes = majorChord.returnNoteVector();
 
 
     // std::vector<Note> noteList = majorChord.at(0).returnNoteVector();
 
-    for (unsigned i=0; i< Notes.size(); i++)
-        std:: cout << Notes.at(i).returnNoteName() << std::endl;
+    // for (unsigned i=0; i< Notes.size(); i++)
+    //     std:: cout << Notes.at(i).returnNoteName() << std::endl;
+
+
+    for (int i=0; i< majorChord.size(); i++){
+        vector<Note> thisGay = majorChord.at(i).returnNoteVector();
+        for (unsigned x=0; x < thisGay.size(); x++){
+            Note thisThing = thisGay.at(x);
+            std::cout << "Chord " + i + thisThing.returnNoteName() << std::endl;
+        }
+        
+    }
+        
     return 0;
 }
