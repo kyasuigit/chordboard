7fa9052e44496fb357ad80013a73c8307145c58c
Kev Yang <kyang267@uwo.ca>
2021-11-30 17:29:56 -0500

    (November 29, 2021) Added doxygen documentation to the hint window class and header file.

--- hintwindow.cpp
+++ hintwindow.cpp
@@ -1,14 +1,16 @@
 #include "hintwindow.h"
 #include "ui_hintwindow.h"
 
-hintWindow::hintWindow(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::hintWindow)
-{
-    ui->setupUi(this);
+/// @file hintWindow.cpp
+/// @brief This class is used to create and another window for displaying a hint to the user for drum parts. The class does not contain anything besides a UI creation and a deconstructor.
+/// @authors Kevin Yang
+
+
+hintWindow::hintWindow(QWidget *parent) :QDialog(parent),ui(new Ui::hintWindow){
+    ui->setupUi(this); // Creating the UI.
 }
 
-hintWindow::~hintWindow()
-{
-    delete ui;
+
+hintWindow::~hintWindow(){
+    delete ui; // Deleting the UI.
 }
--- hintwindow.h
+++ hintwindow.h
@@ -1,6 +1,10 @@
 #ifndef HINTWINDOW_H
 #define HINTWINDOW_H
 
+/// @file hintWindow.h
+/// @brief This class is used to create and another window for displaying a hint to the user for drum parts. The class does not contain anything besides a UI creation and a deconstructor.
+/// @authors Kevin Yang
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,7 +16,18 @@ class hintWindow : public QDialog
     Q_OBJECT
 
 public:
+
+    /// @brief This is the constructor for the class.
+    /// @details This constructor contains a number of QT arguments that are used to create a QWidget for the hint window.
+    /// @param QWidget *parent A pointer to the parent QWidget.
+    /// @param parent
+    /// @param Ui A new Ui.
+    /// @author Kevin Yang
     explicit hintWindow(QWidget *parent = nullptr);
+
+    /// @brief This is the deconstructor for the class.
+    /// @details This deconstructor is responsible for only destroying trhe UI for the hint window.
+    /// @author Kevin Yang
     ~hintWindow();
 
 private:
