22a14513eee13c4c83c685e3df47c52dda889ce7
Terry <t.juottawa@gmail.com>
2021-11-30 19:48:19 -0500

    updated doxygen format

--- drum.h
+++ drum.h
@@ -7,15 +7,15 @@ class drum{
     private:
 
     public:
-        // @brief Constructor for a drum item
-        // @details The constructor initializes the drum object as an empty object
-        // @author Terry Ju
+        /// @brief Constructor for a drum item
+        /// @details The constructor initializes the drum object as an empty object
+        /// @author Terry Ju
         drum();
 
-        // @brief Method returns a vector of integers representing the different notes of drums to be played
-        // @details The method takes in a int representing the length of the song to be played, and outputs a set of drum notes to the frontEnd
-        // @param int length, length of the song to be genereated
-        // @author Terry Ju        
+        /// @brief Method returns a vector of integers representing the different notes of drums to be played
+        /// @details The method takes in a int representing the length of the song to be played, and outputs a set of drum notes to the frontEnd
+        /// @param int length, length of the song to be genereated
+        /// @author Terry Ju        
         std::vector <int> returnNotes(int length);
         
 };
